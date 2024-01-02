f4a269e719c1bc5d2de7b3c6fb359941e7711c97
Kevin.P <kevin.pellissard@gmail.com>
2024-01-02 15:20:50 +0100

    Add task 0 from project 2 async function

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""asynchronous coroutine"""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay = 10):
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
+    return delay
