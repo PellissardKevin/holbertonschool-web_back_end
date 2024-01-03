a9c0300753f4d6d388253db0f8ab0f105574e4b8
Kevin.P <kevin.pellissard@gmail.com>
2024-01-03 16:42:54 +0100

    update task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,6 +10,6 @@ import typing
 async def async_generator() -> typing.Generator[float, None, None]:
     """The coroutine will loop 10 times"""
 
-    for l in range(0, 10):
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
