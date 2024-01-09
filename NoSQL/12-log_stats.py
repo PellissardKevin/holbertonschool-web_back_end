15bc2852f8d8f65fe6f9416391e02c984762a8d5
Kevin.P <kevin.pellissard@gmail.com>
2024-01-09 15:53:14 +0100

    Update execution of task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -10,10 +10,9 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client['logs']
     collection = db['nginx']
-    count = collection.count_documents({})
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    print(f"{count} logs")
+    print(f"{collection.count_documents({})} logs")
     print("Methods:")
     for method in methods:
         count = collection.count_documents({"method": method})
