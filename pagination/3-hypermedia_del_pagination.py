e521c5e1689aee19bcefbea2d8578773a507ebdd
Kevin.P <kevin.pellissard@gmail.com>
2024-01-10 14:37:48 +0100

    Update task 3 project 2

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -49,14 +49,14 @@ class Server:
 
         start_index = index if index is not None else 0
         end_index = start_index + page_size - 1
-        size_data = len(self.dataset())
 
         assert start_index >= 0
-        assert end_index < size_data
+        assert end_index < len(self.dataset())
 
         current_page = [self.dataset()[i] for i in range(start_index,
                                                          min(end_index + 1,
-                                                             size_data))]
+                                                             len(self.dataset(
+                                                             ))))]
 
         next_index = end_index + 1
 
