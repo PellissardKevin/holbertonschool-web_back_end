27045f0bdb5b1bb3fd40650b18ea9275a3d80925
Kevin.P <kevin.pellissard@gmail.com>
2024-01-16 16:41:00 +0100

    update task 7, remove condition for code

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -3,9 +3,6 @@ export default class Airport {
     if (typeof name !== 'string') {
       throw new TypeError('name must be a string');
     }
-    if (typeof code !== 'number') {
-      throw new TypeError('code must be a number');
-    }
     this._name = name;
     this._code = code;
   }
