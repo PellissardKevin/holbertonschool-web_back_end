0e452887f80e38fbedb0d79b867f0351c4396d3c
Kevin.P <kevin.pellissard@gmail.com>
2024-01-29 11:12:38 +0100

    Add task 0 project 1 ES6 data manipulation

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,16 @@
+export default function getListStudents() {
+	let listStudents = ["1,Guillaume,San Francisco",
+		"2,James,Columbia",
+		"5,Serena,San Francisco"
+	];
+	let deck = [];
+
+	for (var i = 0; i < listStudents.length; i++) {
+		const listStudentsArray = listStudents[i].split(',');
+		const id = listStudentsArray[0];
+		const firstName = listStudentsArray[1];
+		const location = listStudentsArray[2];
+		deck.push({ id, firstName, location });
+	}
+	return deck;
+};
